fdef423423c172dcc649d457d9add0f8fcbd6bcf
Dennis Antwi-Buadum <csrqwe13@gmail.com>
2024-02-08 09:47:41 -0600

    create dist directory with project

--- src/Scripts/App.js
+++
@@ -1,6 +0,0 @@
-import {storage} from "./WebStorageAPI.js";
-import { loadProjects } from "./Project-DOM-Loading.js";
-import { loadTodoList } from "./Todo-DOM-Loading.js";
-
-loadProjects();
-loadTodoList(storage[0]);
